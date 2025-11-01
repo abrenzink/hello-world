cedcd5f7f4bbd55bde92d26bce8355cdfd57372c
abrenzink <abrenzink@bonadmin.com>
2025-11-01 15:20:42 -0300

    fix: variable name

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-const message: string = "Hello World by Andrea Brenzink from TypeScript! 🎉";
-console.log(message);
-
 let firstName: string = 'Andrea';
 let fullName: string = `${firstName} Brenzink`;
-let age: number = 32;
 
-console.log('Name: ' + fullName);
-console.log('Age: ' + age);
+const message: string = `Hello World by ${fullName} from TypeScript! 🎉`;
+console.log(message);
